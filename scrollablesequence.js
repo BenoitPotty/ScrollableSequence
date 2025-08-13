c9bd525ba74a58917ab3eb0b602056d937e5489e
Benoit Potty <benoit.potty@liip.ch>
2025-08-13 16:46:18 +0200

    Fix bug when included in a full page

--- scrollablesequence.js
+++ scrollablesequence.js
@@ -1,52 +1,79 @@
 class ScrollableSequence {
-  
   constructor(canvasId) {
     this.canvasId = canvasId;
-    this.replacementToken = '{framenumber}';
+    this.replacementToken = "{framenumber}";
   }
 
   init = () => {
     this.documentElement = document.documentElement;
-  
+
     this.canvasElement = document.getElementById(this.canvasId);
     if (!this.canvasElement) {
       throw `Canvas HTMLElement with id ${this.canvasId} was not found.`;
     }
-    
+
     this.frameCount = this.canvasElement.dataset.framecount;
     if (!this.frameCount) {
       throw `data-framecount attribute not set on the canvas.`;
-    };
-    
+    }
+
     this.imagePath = this.canvasElement.dataset.imagepath;
     if (!this.imagePath) {
       throw `data-imagepath attribute not set on the canvas`;
-    } else if (!this.imagePath.includes(this.replacementToken)){
+    } else if (!this.imagePath.includes(this.replacementToken)) {
       throw `Replacement token ${this.replacementToken} not found in the imagePath`;
     }
-    
-    
-    this.frameNumberPading = Number.parseInt(this.canvasElement.dataset.framenumberpading);
+
+    this.frameNumberPading = Number.parseInt(
+      this.canvasElement.dataset.framenumberpading
+    );
     if (!this.frameNumberPading) this.frameNumberPading = 0;
     
+    this.animationSpeedFactor = Number.parseInt(
+      this.canvasElement.dataset.speed
+    );
+    if (!this.animationSpeedFactor) this.animationSpeedFactor = 3;
+
     this.canvasContext = this.canvasElement.getContext("2d");
-    
+
     this.preloadImages();
 
     this.positionCanvasRelativelyToItsParent();
-  
+
     this.sizeCanvasToParentSize();
-  
+
     this.buildFirstImage();
-  
-    this.subscribeToScrollEvent();
+
+    this.initialiseObserver();
+
+    this.moveSiblingFarEnough();
 
     this.subscribeToWindowResize();
   };
 
+  initialiseObserver() {
+    let viewportHeight = window.innerHeight;
+    let canvasHeight = this.canvasElement.height;
+    this.obersvationMargin = Math.round((viewportHeight - canvasHeight) / 2);
+    let options = {
+      rootMargin: `0px 0px -${this.obersvationMargin}px 0px`,
+      threshold: 1.0,
+    };
+
+    const canvasObserver = new IntersectionObserver((entries) => {
+      console.dir(entries);
+      if (entries[0].isIntersecting) {
+        console.log("intersecting");
+        this.startAnimation();
+      }
+    }, options);
+
+    let target = document.getElementById(this.canvasId);
+    canvasObserver.observe(target);
+  }
 
   positionCanvasRelativelyToItsParent() {
-    this.canvasElement.style.position = 'relative';
+    this.canvasElement.style.position = "relative";
     this.canvasElement.style.top = 0;
     this.canvasElement.style.left = 0;
   }
@@ -57,16 +84,19 @@ class ScrollableSequence {
       img.src = this.buildImageName(i);
     }
   };
-  
+
   buildImageName = (index) => {
-    return `${this.imagePath.replace(this.replacementToken, index.toString().padStart(this.frameNumberPading, '0'))}`;
+    return `${this.imagePath.replace(
+      this.replacementToken,
+      index.toString().padStart(this.frameNumberPading, "0")
+    )}`;
   };
-  
+
   updateImage = (index) => {
     this.imageObject.src = this.buildImageName(index);
     this.drawImageToFit();
   };
-  
+
   buildFirstImage = () => {
     this.imageObject = new Image();
     this.imageObject.src = this.buildImageName(1);
@@ -76,24 +106,55 @@ class ScrollableSequence {
   };
 
   drawImageToFit() {
-    var scale = Math.min(this.canvasElement.width / this.imageObject.width, this.canvasElement.height / this.imageObject.height);
-    var x = (this.canvasElement.width / 2) - (this.imageObject.width / 2) * scale;
-    var y = (this.canvasElement.height / 2) - (this.imageObject.height / 2) * scale;
-    this.canvasContext.drawImage(this.imageObject, x, y, this.imageObject.width * scale, this.imageObject.height * scale);
+    var scale = Math.min(
+      this.canvasElement.width / this.imageObject.width,
+      this.canvasElement.height / this.imageObject.height
+    );
+    var x = this.canvasElement.width / 2 - (this.imageObject.width / 2) * scale;
+    var y =
+      this.canvasElement.height / 2 - (this.imageObject.height / 2) * scale;
+    this.canvasContext.drawImage(
+      this.imageObject,
+      x,
+      y,
+      this.imageObject.width * scale,
+      this.imageObject.height * scale
+    );
   }
-  
-  subscribeToScrollEvent = () => {
-    window.addEventListener("scroll", () => {
+
+  startAnimation = () => {
+    this.setParentPosition();
+    
+    window.addEventListener("scroll", (event) => {
       const scrollTop = this.documentElement.scrollTop;
-      const maxScrollTop = this.documentElement.scrollHeight - window.innerHeight;
+      const maxScrollTop =
+        this.documentElement.scrollHeight - window.innerHeight;
       const scrollFraction = scrollTop / maxScrollTop;
       const frameIndex = Math.min(
         this.frameCount - 1,
         Math.ceil(scrollFraction * this.frameCount)
       );
-  
+
       requestAnimationFrame(() => this.updateImage(frameIndex + 1));
     });
+  };
+
+  setParentPosition = () => {
+    let parentStyle = this.canvasElement.parentElement.style;
+    let parentCoordinates = this.canvasElement.parentElement.getBoundingClientRect();
+    parentStyle.top = `${parentCoordinates.top}px`;
+    parentStyle.left = `${parentCoordinates.left}px`;
+    parentStyle.position = 'fixed';
+  }
+
+  moveSiblingFarEnough = () => {
+    let nextSibling = this.canvasElement.parentElement.nextElementSibling;
+    nextSibling.style.marginTop = `${((this.frameCount * this.canvasElement.height) / this.animationSpeedFactor) + 500}px`;
+    
+  }
+
+  unsubscribeToScrollEvent = () => {
+    window.removeEventListener("scroll");
   };
 
   subscribeToWindowResize = () => {
@@ -101,10 +162,10 @@ class ScrollableSequence {
       this.sizeCanvasToParentSize();
       this.drawImageToFit();
     });
-  }
-  
+  };
+
   sizeCanvasToParentSize = () => {
     this.canvasElement.width = this.canvasElement.parentElement.offsetWidth;
     this.canvasElement.height = this.canvasElement.parentElement.offsetHeight;
-  }
+  };
 }
